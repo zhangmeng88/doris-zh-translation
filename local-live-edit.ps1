c2bcae7b6b2d7a4b8d474a8a84a8e3c0596f09be
Can Celik <can.celik@outlook.com>
2023-06-13 12:03:11 +0200

    added language folder

--- local-live-edit.ps1
+++ local-live-edit.ps1
@@ -2,7 +2,7 @@
 docker run -ti `
            --name doris-mkdocs `
            -p 8080:8000 `
-           -v C:/code/GitRepos/Docs/doris:/mkdocs `
+           -v C:/code/GitRepos/Docs/doris/en:/mkdocs `
            -v C:/code/gitrepos/DocsBuilder/docbuilder/custom_readthedocs:/mkdocs/custom_readthedocs `
            --env LIVE_RELOAD_SUPPORT=true `
            polinux/mkdocs 
